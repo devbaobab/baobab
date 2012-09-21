01cf2feff1c9d59b32be1c32424b733f8099c6ef
devbaobab <dev.baobabclass@gmail.com>
2012-09-21 10:57:01 +0900

    added config and database schema

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baobab::Application.config.secret_token = '30771e69c1483e464d4287174f103873baa14db144de2d1a3c9b80f09381e87ecdf0b583625398df6e616fae39d4aa993651db68435f88b1911acd0f845d699b'
