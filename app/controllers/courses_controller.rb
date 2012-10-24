30922bb6fae9e43df4268fb55d5fcc3ccb57a473
devbaobab <dev.baobabclass@gmail.com>
2012-10-25 01:32:38 +0900

    updated courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -78,7 +78,7 @@ class CoursesController < ApplicationController
     authorize! :create, @course
     # @course = Course.create(params[:course])
     # @chapter = @course.chapters.build(params[:chapter])
-    @course.users << current_user
+    @course.authours << current_user
     respond_to do |format|
       if @course.save
         format.html { redirect_to @course, notice: 'Course was successfully created.' }
