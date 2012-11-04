38bdfaa01baa9624a7d6f42273931314b7474d91
kkb2849 <kkb2849@daum.net>
2012-11-05 01:20:43 +0900

    add show error details module to application.rb

--- config/application.rb
+++ config/application.rb
@@ -60,5 +60,8 @@ module Baobab
     config.assets.precompile += ['common.css']
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.consider_all_requests_local       = true
+    
   end
 end
