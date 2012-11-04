8f2428453eacc560be94afe32c187d0fe5dfc3e5
kkb2849 <kkb2849@daum.net>
2012-11-04 17:33:34 +0900

    enable error page in production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,8 @@ Baobab::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
+  config.consider_all_requests_local = true
+  
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
